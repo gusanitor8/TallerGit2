51ec86f0a6faaa9881ca14e2dee9787d41f7f46a
gusanitor8 <gusanitor8@gmail.com>
2024-04-25 13:02:54 -0600

    Nuevo metodo

--- Main.java
+++ Main.java
@@ -2,4 +2,13 @@ class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+
+    public static void printHello(){
+        String cadena = "Hello";
+
+        for (int i = 0; i < 5; i++){
+            System.out.print(cadena.charAt(i));            
+        }
+        System.out.println();
+    }
 }
